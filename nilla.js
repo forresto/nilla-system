fbf7f4c9765ad0e7a508b64b1f27d8ce0f9e03fa
Forrest Oliphant <github.com@sembiki.com>
2015-09-13 17:25:03 +0200

    alpha

---
+++ nilla.js
@@ -0,0 +1,27 @@
+// Just wrap each item in a section, and output the HTML as provided by API
+// No CSS/styling is performed
+window.polySolvePage = function(page, options, callback) {
+    var line = "\n";
+    var ind = "  ";
+
+    var html = "<!doctype html>\n<html><head><meta charset=\"utf-8\">";
+    html += line + ind + '<link rel="stylesheet" href="http://d2v52k3cl9vedd.cloudfront.net/basscss/7.0.4/basscss.min.css">';
+    html += line + "</head><body>";
+
+    // Debug
+    html += line + '<!--';
+    html += JSON.stringify(arguments);
+    html += line + '-->';
+
+    page.items.forEach(function(item) {
+        html += line+ind+"<section>";
+        item.content.forEach(function(block) {
+            html += line+ind+ind+block.html;
+        });
+        html += line+ind+"</section>"
+    });
+    html += line+"</body></html>"
+    var err = null;
+    var details = {};
+    return callback(err, html, details);
+};
